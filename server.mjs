28595360716a8cd8204a77c90cfe56bcbfc2c039
Karen Guo <karengu0@live.unc.edu>
2022-04-19 16:58:26 -0400

    6

--- server.mjs
+++ server.mjs
@@ -1,4 +1,117 @@
-import { coinFlips, countFlips, coinFlip, flipACoin } from "./modules/coin.mjs";
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+// function getRandomIntInclusive(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  }
+  
+  /** Multiple coin flips
+   * 
+   * Write a function that accepts one parameter (number of flips) and returns an array of 
+   * resulting "heads" or "tails".
+   * 
+   * @param {number} flips 
+   * @returns {string[]} results
+   * 
+   * example: coinFlips(10)
+   * returns:
+   *  [
+        'heads', 'heads',
+        'heads', 'tails',
+        'heads', 'tails',
+        'tails', 'heads',
+        'tails', 'heads'
+      ]
+   */
+  
+  function coinFlips(flips) {
+    let resultsArray = [];
+    for (var i = 0; i < flips; i++) {
+      let flip = coinFlip();
+      resultsArray[i] = flip;
+    }
+    return resultsArray;
+  }
+  
+  /** Count multiple flips
+   * 
+   * Write a function that accepts an array consisting of "heads" or "tails" 
+   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+   * an object containing the number of each.
+   * 
+   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+   * { tails: 5, heads: 5 }
+   * 
+   * @param {string[]} array 
+   * @returns {{ heads: number, tails: number }}
+   */
+  
+  function countFlips(array) {
+    let heads = 0;
+    let tails = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]=='heads') {
+        heads++;
+      } else if (array[i] == 'tails') {
+        tails++;
+      }
+    }
+    if (heads == 0) {
+      return {"tails": tails};
+    } else if (tails == 0) {
+      return {"heads": heads};
+    }
+    return {"heads": heads, "tails": tails};
+  }
+  
+  /** Flip a coin!
+   * 
+   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+   * 
+   * @param {string} call 
+   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+   * 
+   * example: flipACoin('tails')
+   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+   */
+  
+  function flipACoin(call) {
+    let flip = coinFlip();
+    let result = "";
+    if (flip = call) {
+      result = "win";
+    } else if (flip != call) {
+      result = "lose";
+    }
+    return {"call": call, "flip": flip, "result": result};
+  }
+  
+  
+  /** Export 
+   * 
+   * Export all of your named functions
+  */
+  
+  // export {coinFlip, coinFlips, countFlips, flipACoin};
+  
+
 import minimist from 'minimist'; // parses argument options
 import express from 'express'; // minimal & flexible Node.js web application framework
                                 // provides features for web & mobile applications
@@ -7,6 +120,8 @@ import express from 'express'; // minimal & flexible Node.js web application fra
 
 // Require minimist module
 const args = require('minimist')(process.argv.slice(2))
+// Require Express.js
+const express = require('express')
 // See what is stored in the object produced by minimist
 console.log(args)
 // Store help text 
