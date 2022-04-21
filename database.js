e6eca4d80a150f7ee0a5ac62d5fd32a803b02597
Karen Guo <karengu0@live.unc.edu>
2022-04-21 17:44:09 -0400

    database

--- database.js
+++ database.js
@@ -4,11 +4,11 @@
 const Database = require('better-sqlite3');
 
 // Connect to a database or create one if it doesn't exist yet.
-const db = new Database('user.db');
+const db = new Database('log.db');
 
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
     );
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
@@ -18,8 +18,11 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
-        INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
+        TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY, 
+            remoteaddr TEXT, remoteuser TEXT, time INTEGER, 
+            method TEXT, url TEXT, protocol TEXT, 
+            httpversion TEXT, status INTEGER, 
+            referer TEXT, useragent TEXT);
     `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
